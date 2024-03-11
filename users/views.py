4aa05418cde03e4cea79502454337984ebe8487c
MohamedAbdElgni <mohamed.kk878@gmail.com>
2024-03-11 22:23:59 +0200

    test connection end point

--- users/views.py
+++ users/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+
+
+
+
+# simple end point to return a string for rest api framework
+
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.decorators import api_view
+
+@api_view(['GET'])
+def test_connection(request):
+    return Response("congratulations, server is working!", status=200)
+
