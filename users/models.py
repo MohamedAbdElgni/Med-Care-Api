dad4b91672d62c2384d62342ebb64e6fe6073eb0
Israa Lotfy <israalotfy891@gmail.com>
2024-03-12 14:43:15 +0200

    update models

--- users/models.py
+++ users/models.py
@@ -39,4 +39,10 @@ class Doctor(models.Model):
     def __str__(self):
         return self.user.username
     
-    
+#rating    
+class Rating(models.Model):
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    comment = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)  
