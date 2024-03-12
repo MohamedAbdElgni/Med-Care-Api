f95cffc583bd64e635cd68da9af4341936dafd74
MohamedAbdElgni <mohamed.kk878@gmail.com>
2024-03-12 04:04:24 +0200

    auth

--- project/settings.py
+++ project/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'users',
     'rest_framework',
+    'knox',
     
 ]
 
@@ -124,3 +125,11 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'users.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'knox.auth.TokenAuthentication',
+    ],
+    
+}
