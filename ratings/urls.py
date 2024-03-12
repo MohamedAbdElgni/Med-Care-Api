4ba883704d6a9d212b36c79271373ea2d6bc01f2
Israa Lotfy <israalotfy891@gmail.com>
2024-03-13 00:53:23 +0200

    update

--- ratings/urls.py
+++ ratings/urls.py
@@ -5,7 +5,8 @@ urlpatterns = [
     #rating
     path('ratings/', all_ratings, name='all-ratings'), 
     path('ratings/<int:rating_id>/', rating_detail, name='rating-detail'), 
-
+    #update
+    path('ratings/doctor/<int:doctor_id>/', doctor_ratings, name='doctor-ratings'),
 
 
 ]
