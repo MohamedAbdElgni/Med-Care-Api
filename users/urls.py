483c0894373c4d73d7e5b461887bb4da57397cca
Israa Lotfy <israalotfy891@gmail.com>
2024-03-20 19:00:49 +0200

    modify url for changepassword

--- users/urls.py
+++ users/urls.py
@@ -12,5 +12,6 @@ urlpatterns = [
     path('users/<int:id>/', get_user, name='get_user'),
     path('test/', test, name='test'),
     path('activate/<uidb64>/<token>/', activate, name='activate'),
+    path('users/change-password/<int:id>/', ChangePasswordView.as_view(), name='change_password'),
 
 ]
