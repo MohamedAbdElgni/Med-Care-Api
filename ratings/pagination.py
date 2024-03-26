78a8d3c6b967dbf8e79f93a28a8e72b63efbb9c2
Yusuf Abu-Alam <yabualam@gmail.com>
2024-03-26 23:32:11 +0200

    adding pagination to rating

---
+++ ratings/pagination.py
@@ -0,0 +1,22 @@
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
+
+class CustomPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 100
+    page_query_param = 'p'
+
+    def get_page_size(self, request):
+        try:
+            return int(request.query_params.get(self.page_size_query_param, self.page_size))
+        except (TypeError, ValueError):
+            return self.page_size
+
+    def get_paginated_response(self, data):
+        return Response({
+            'count': self.page.paginator.count,
+            'next': self.get_next_link(),
+            'previous': self.get_previous_link(),
+            'results': data
+        })
